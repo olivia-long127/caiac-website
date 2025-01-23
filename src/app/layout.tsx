388589c9882eed9cde03bf243778970df4f55da1
Ryan HY Zhang <65325330+Rhyzhang@users.noreply.github.com>
2025-01-23 00:46:43 -0500

    Update layout.tsx: Google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
 
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GJXS1B9ZXK"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-GJXS1B9ZXK');
+        </script>
+
       </head>
       
       <body>
